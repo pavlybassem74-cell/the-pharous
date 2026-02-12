f8ca62e7539ac8b93ef6b9983e9ee251db0e915a
pavlybassem74-cell <pavlybassem74@gmail.com>
2026-02-12 20:29:50 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,36 @@
-function requestAccess(){
-  const name = document.getElementById("name").value;
-  if(name){
-    document.getElementById("status").innerText =
-    "Waiting for Pharaoh Approval...";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB4WRSJkZxebzoZUBAoXEpSoYW1AU7La5w",
+  authDomain: "the-pharous.firebaseapp.com",
+  projectId: "the-pharous",
+  storageBucket: "the-pharous.firebasestorage.app",
+  messagingSenderId: "990089886334",
+  appId: "1:990089886334:web:681e2115254ea73ce3362f"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.requestAccess = async function () {
+  const name = document.getElementById("nameInput").value;
+
+  if (!name) {
+    alert("Please enter your name");
+    return;
   }
-}
+
+  try {
+    await addDoc(collection(db, "requests"), {
+      name: name,
+      time: new Date(),
+      approved: false
+    });
+
+    alert("Request sent successfully 👑");
+  } catch (error) {
+    alert("Error sending request");
+    console.error(error);
+  }
+};
