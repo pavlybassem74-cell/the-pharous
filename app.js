275c7d6c742bd33bdd54ac4c0e36820958e72aec
pavlybassem74-cell <pavlybassem74@gmail.com>
2026-02-13 01:00:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebas
 import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB4WRSJkZxebzoZUBAoXEpSoYW1AU7La5w",
+  apiKey: "AIzaSyB4VRsJkZxebzoZUBA0XEpSoYW1AU7La5w",
   authDomain: "the-pharous.firebaseapp.com",
   projectId: "the-pharous",
-  storageBucket: "the-pharous.firebasestorage.app",
+  storageBucket: "the-pharous.appspot.com",
   messagingSenderId: "990089886334",
   appId: "1:990089886334:web:681e2115254ea73ce3362f"
 };
@@ -14,23 +14,22 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 window.requestAccess = async function () {
-  const name = document.getElementById("nameInput").value;
+  const name = document.querySelector("input").value;
 
   if (!name) {
-    alert("Please enter your name");
+    alert("Enter your name");
     return;
   }
 
   try {
     await addDoc(collection(db, "requests"), {
       name: name,
-      time: new Date(),
-      approved: false
+      createdAt: new Date()
     });
 
-    alert("Request sent successfully 👑");
+    alert("Request Sent 👑");
   } catch (error) {
-    alert("Error sending request");
     console.error(error);
+    alert("Error sending request");
   }
 };
